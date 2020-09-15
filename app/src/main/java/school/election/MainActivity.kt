0610dfca929e7283ecf2a744e4b60352b8cac1cb
2003l <2002.leonid2003@gmail.com>
2020-09-15 18:51:57 +0300

    cool shit

--- app/src/main/java/school/election/MainActivity.kt
+++ app/src/main/java/school/election/MainActivity.kt
@@ -1,6 +1,5 @@
 package school.election
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
---
+++ app/src/main/java/school/election/VoteActivity.kt
@@ -0,0 +1,11 @@
+package school.election
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vote)
+    }
+}
